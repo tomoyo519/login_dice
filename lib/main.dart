99d65a2c4fdf4a97fbe9573d364231ffa6454bc0
tomoyo519 <tomoyo519@naver.com>
2022-02-22 00:30:37 +0900

    practice login page with input tag

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,13 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class LogIn extends StatelessWidget {
+class LogIn extends StatefulWidget {
+  @override
+  _LogInState createState() => _LogInState();
+}
+class _LogInState extends State<LogIn>{
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,16 +28,48 @@ class LogIn extends StatelessWidget {
         backgroundColor: Colors.redAccent,
         centerTitle: true,
         leading: IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: (){}
-        ),
+          icon: Icon(Icons.menu), onPressed: () {}),
         actions: <Widget>[
           IconButton(
-              icon: Icon(Icons.search),
-              onPressed: (){}
-          )
+            icon: Icon(Icons.search), onPressed: () {})
         ],
       ),
+      body : Column(children: <Widget>[
+        Padding(padding: EdgeInsets.only(top:50)),
+        Center(
+        child: Image(
+          image: AssetImage('image/chef.gif'),
+          width: 170.0,
+          height: 190.0),
+
+      ),
+      Form(
+        child: Theme(
+          data: ThemeData(
+            primaryColor: Colors.teal,
+            inputDecorationTheme: InputDecorationTheme(
+              labelStyle: TextStyle(color: Colors.teal,
+              fontSize: 15.0)
+            )
+          ),
+          child:Container(
+            padding: EdgeInsets.all(40.0),
+            child: Column(children:  <Widget>[
+              TextField(decoration: InputDecoration(labelText:'Enter dice',),
+              keyboardType: TextInputType.emailAddress,)
+              , TextField(
+                decoration: InputDecoration(
+                  labelText: 'Enter Password'
+                ),
+                keyboardType: TextInputType.text,
+                obscureText: true,
+              )
+              ],
+              
+            )
+          ))
+          )
+      ],)
     );
   }
 }
