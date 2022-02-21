37cf631be520a8fa79a14d758d3ce6b796aa4b04
tomoyo519 <tomoyo519@naver.com>
2022-02-22 01:09:08 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,60 +17,72 @@ class LogIn extends StatefulWidget {
   @override
   _LogInState createState() => _LogInState();
 }
-class _LogInState extends State<LogIn>{
 
-
+class _LogInState extends State<LogIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Log in'),
-        backgroundColor: Colors.redAccent,
-        centerTitle: true,
-        leading: IconButton(
-          icon: Icon(Icons.menu), onPressed: () {}),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search), onPressed: () {})
-        ],
-      ),
-      body : Column(children: <Widget>[
-        Padding(padding: EdgeInsets.only(top:50)),
-        Center(
-        child: Image(
-          image: AssetImage('image/chef.gif'),
-          width: 170.0,
-          height: 190.0),
+        appBar: AppBar(
+          title: Text('Log in'),
+          backgroundColor: Colors.redAccent,
+          centerTitle: true,
+          leading: IconButton(icon: Icon(Icons.menu), onPressed: () {}),
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.search), onPressed: () {})
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Padding(padding: EdgeInsets.only(top: 50)),
+              Center(
+                child: Image(
+                    image: AssetImage('image/chef.gif'),
+                    width: 170.0,
+                    height: 190.0),
+              ),
+              Form(
+                  child: Theme(
+                      data: ThemeData(
+                          primaryColor: Colors.teal,
+                          inputDecorationTheme: InputDecorationTheme(
+                              labelStyle: TextStyle(
+                                  color: Colors.teal, fontSize: 15.0))),
+                      child: Container(
+                          padding: EdgeInsets.all(40.0),
+                          child: Column(
+                            children: <Widget>[
+                              TextField(
+                                decoration: InputDecoration(
+                                  labelText: 'Enter dice',
+                                ),
+                                keyboardType: TextInputType.emailAddress,
+                              ),
+                              TextField(
+                                decoration: InputDecoration(
+                                    labelText: 'Enter Password'),
+                                keyboardType: TextInputType.text,
+                                obscureText: true,
+                              )
+                              ButtonTheme(
+                                minWidth: 100.0,
+                              height: 50.0,
+                             
+                              child: ElevatedButton(
+                             
+                              child: Icon(
 
-      ),
-      Form(
-        child: Theme(
-          data: ThemeData(
-            primaryColor: Colors.teal,
-            inputDecorationTheme: InputDecorationTheme(
-              labelStyle: TextStyle(color: Colors.teal,
-              fontSize: 15.0)
-            )
+                            Icons.arrow_forward,
+                            color: Colors.white,
+                            size:35.0),
+                              
+
+                                onPressed: (){}))
+                            ]
+                            ,
+                          ))))
+            ],
           ),
-          child:Container(
-            padding: EdgeInsets.all(40.0),
-            child: Column(children:  <Widget>[
-              TextField(decoration: InputDecoration(labelText:'Enter dice',),
-              keyboardType: TextInputType.emailAddress,)
-              , TextField(
-                decoration: InputDecoration(
-                  labelText: 'Enter Password'
-                ),
-                keyboardType: TextInputType.text,
-                obscureText: true,
-              )
-              ],
-              
-            )
-          ))
-          )
-      ],)
-    );
+        ));
   }
 }
-
